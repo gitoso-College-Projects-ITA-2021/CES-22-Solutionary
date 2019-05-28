0cb297af33b8023cc926bac8c68e9e8ef03c100e
lucasbeto <skybilobran@gmail.com>
2019-05-28 20:42:13 -0300

    First routes

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'It works!'
+
+@app.route("/name/<name>")
+def get_book_name(name):
+    return "name : {}".format(name)
+
+@app.route("/details")
+def get_book_details():
+    author=request.args.get('author')
+    published=request.args.get('published')
+    return "Author : {}, Published: {}".format(author,published)
+
+if __name__ == '__main__':
+    app.run()
