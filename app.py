ba626e8cbc0f965ba11680f977005b24914131f9
lucasbeto <skybilobran@gmail.com>
2019-05-31 15:12:25 -0300

    Add login authentication functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 
 from wtform_fields import *
 from models import *
@@ -14,16 +15,48 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://unuolholzadzsf:b2476c2787249
 
 db = SQLAlchemy(app)
 
+# Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+
+    return User.query.get(int(id))
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     login_form = LoginForm()
     app.jinja_env.globals['login_form'] = login_form
     app.jinja_env.globals['logged'] = False
 
+    # Allow login if validation success
     if login_form.validate_on_submit():
         app.jinja_env.globals['logged'] = True
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        if current_user.is_authenticated:
+            return "Logged in with flask-login!"
+
         return render_template("index.html")
+
     return render_template("index.html")
+
+@app.route("/private", methods=['GET', 'POST'])
+@login_required
+def private():
+
+    # Usar isso se na for usar o "@login_required"
+    #if not current_user.is_authenticated:
+    #    return "Please login."
+    return "something"
+
+@app.route("/logout", methods=['GET'])
+def logout():
+
+    logout_user()
+    return "Logged out"
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
